2817b18f8e01d3c9496288f17145f79244c8fc78
psiorx <psiorx@gmail.com>
2015-10-26 00:40:06 -0400

    added kalman filter for sensor fusion and bias estimation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "gyroscope.h"
 #include "accelerometer.h"
+#include "Kalman.h"
 
 using namespace std;
 using namespace Eigen;
@@ -10,11 +11,13 @@ int main()
   Gyroscope gyro(1);
   Accelerometer accel(2);
   
-  cout << gyro.Calibrate() << endl;
-  
+  cout << gyro.Calibrate(500) << endl;
+  Kalman kalman_filter;
+
   for(int x = 0; x< 50000; x++) {
-    float pitch = Accelerometer::ComputePitchFast(accel.Read());
-    cout << pitch << endl;
+    float pitch_accel = Accelerometer::ComputePitchFast(accel.Read()) * 180 / M_PI;
+  	float pitch = kalman_filter.getAngle(pitch_accel, gyro.Read(), 0.01f);
+  	cout << pitch << endl;
   }
 
   return 0;
